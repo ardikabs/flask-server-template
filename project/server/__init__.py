d3daf32d93c41e9062c7619284e7f99d86bfe431
ardikabs <ardikabs@gmail.com>
2018-12-22 15:26:59 +0700

    edit make_celery func

--- project/server/__init__.py
+++ project/server/__init__.py
@@ -10,16 +10,17 @@ from server import main
 def make_server(config_name=None, gunicorn=False):
     application = create_app(config_name)
     configure_logger(application, gunicorn=gunicorn)
+
     main.init_app(application)
     application.app_context().push()
     return application
 
-def make_worker(config_name=None, gunicorn=False):
-    application = create_app()
+def make_worker(config_name=None):
+    application = create_app(config_name)
     application.app_context().push()
 
     celery = create_celery(application)
-
     from server.extensions.celery import tasks
     tasks.setup()
+
     return celery
